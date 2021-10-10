27e7eb92e1fe4b52e254932ba220151a835a8e81
gizm00 <4462255+gizm00@users.noreply.github.com>
2021-10-10 15:24:04 -0700

    updates

--- test/install-airflow.sh
+++ test/install-airflow.sh
@@ -1,4 +1,6 @@
-sleep 4s
+export AIRFLOW__WEBSERVER__WORKER_REFRESH_BATCH_SIZE=0
+export AIRFLOW__WEBSERVER__WORKER_REFRESH_INTERVAL=0
+export AIRFLOW__WEBSERVER__WORKERS=2
 pip install -r requirements.txt --ignore-installed
 airflow db init
 airflow users create \
